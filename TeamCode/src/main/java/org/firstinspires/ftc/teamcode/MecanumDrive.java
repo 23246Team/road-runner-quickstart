feef000fd754ed5863b0fb75efb8711193fbad2f
ErrorCode23246 <149131927+ErrorCode23246@users.noreply.github.com>
2025-08-19 09:56:30 -0700

    Updated Tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -70,7 +70,7 @@ public final class MecanumDrive {
         // feedforward parameters (in tick units)
         public double kS = 1.1176889959133862;
         public double kV = 0.0006267231990393737;
-        public double kA = 0;
+        public double kA = 0.0001;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
